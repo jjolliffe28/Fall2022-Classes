5145a4e80c308ee2e7ddf54823d93258889c8611
Jack Jolliffe <jolliffejack@gmail.com>
2022-09-13 11:58:54 -0400

    new notes

---
+++ Fall2022-Classes/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Fall2022_Classes
+{
+    public class Car
+    {
+	    // Properties
+
+    public string Make { get; set; }    
+
+	public string Model { get; set; }
+
+	public string Color { get; set; }
+    
+    public int Year { get; set; }
+
+    public int Speed { get; set; }
+
+    public bool IsPaidFor { get; set; }
+
+        // Shortcut to create these is prop TAB TAB
+
+        // access modifiers: public, private, internal
+        // different ways we can access classes, properties, methods, etc in our application
+        // If these properties were private we could not access "make" for example in our Programs.cs file
+        // We use public here because it allows us to directly access our properties 
+        // the Get allows us to use the dot operator and set allows us to change the value
+
+
+
+
+        // Constructors
+
+        public Car()
+        {
+            Make = "Lexus";
+	        Model = "RX 350";
+            Color = "White";
+            Year = 2018;
+            Speed = 0;
+            IsPaidFor = false;
+        }
+	   
+        public Car(string make, string model, string color, int year) 
+	    {
+            Make = make;
+            Model = model;
+            Color = color;
+            Year = year;
+	    }
+
+        // Methods
+
+
+
+    }
+}
+
---
+++ Fall2022-Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using Fall2022_Classes;
+
+Car ashleysCar = new Car();
+Console.WriteLine("Ashley has a " + ashleysCar.Make);
+
+ashleysCar.IsPaidFor = true;
+Console.WriteLine("This car is paid for? : " + ashleysCar.IsPaidFor);
+
+Car gavinsCar = new Car("Infiniti", "G37x", "Black", 2009);
+Console.WriteLine("Gavin has a " + gavinsCar.Make);
+
+
+// making a list of objects from our new class
+
+var garage = new List<Car>();
+
+garage.Add(ashleysCar);
+garage.Add(gavinsCar);
+
+Console.WriteLine(garage[0].Model);
